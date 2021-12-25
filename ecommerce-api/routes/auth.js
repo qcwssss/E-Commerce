efaeb9076305669b4ad445fb96edc8a21125179a
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-25 10:28:23 -0800

    hide password when login

--- ecommerce-api/routes/auth.js
+++ ecommerce-api/routes/auth.js
@@ -8,18 +8,39 @@ router.post('/register', async (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: CryptoJS.AES.encrypt(
-            req.body.password, 
+            req.body.password,
             process.env.PASS_SECRET
-            ).toString(),        
+        ).toString(),
     });
 
     try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
-        // console.log(savedUser);
-    } catch(err) {
+    } catch (err) {
         res.status(500).json(err);
-        // console.log(err);
+    }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        });
+        !user && res.status(401).json('Wrong credentials! Could not find user.');
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SECRET
+        );
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        OriginalPassword !== req.body.password && res.status(401).json('Invalid password');
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
