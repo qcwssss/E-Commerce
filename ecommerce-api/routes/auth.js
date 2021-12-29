a457513fc019eb4d400c660fbd75b41f979db1d0
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-28 20:33:54 -0800

    add users router method

--- ecommerce-api/routes/auth.js
+++ ecommerce-api/routes/auth.js
@@ -12,6 +12,7 @@ router.post('/register', async (req, res) => {
             req.body.password,
             process.env.PASS_SECRET
         ).toString(),
+        isAdmin: req.body.isAdmin
     });
 
     try {
--- ecommerce-api/routes/user.js
+++ ecommerce-api/routes/user.js
@@ -1,8 +1,8 @@
-const { verifyToken, verifyTokenAndAuthorization } = require('../middleware/verifyToken');
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('../middleware/verifyToken');
 const router = require('express').Router();
 const User = require('../models/User');
 
-// REGISTER
+// UPDATE
 router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
     if (req.body.password) {
         req.body.password = CryptoJS.AES.encrypt(
@@ -21,9 +21,42 @@ router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
         res.status(200).json(updatedUser);
     } catch(err) {
         res.status(500).json(err);
-    }
-
-    
+    }    
 });
+
+// DELETE
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json('User has been deleted...');
+    } catch {
+        res.status(500).json(err)
+    }
+});
+
+// GET
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch {
+        res.status(500).json(err)
+    }
+});
+
+// GET ALL USERS
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+    const query = req.params.new;
+    try {
+        const users = query 
+            ? await User.find().sort({ _id: -1 }).limit(5) 
+            : await User.find();
+        res.status(200).json(users);
+    } catch {
+        res.status(500).json(err)
+    }
+});
+
 
 module.exports = router;
