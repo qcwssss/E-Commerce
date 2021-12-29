db1510728989c9eacd7a2187da9994bd7239e4d4
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-29 10:51:04 -0800

    update post request of api/products

--- ecommerce-api/routes/product.js
+++ ecommerce-api/routes/product.js
@@ -1,3 +1,22 @@
+const Product = require('../models/Product');
+const {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin
+} = require('../middleware/verifyToken');
+
 const router = require('express').Router();
+
+// CREATE
+router.post('/', verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body);
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
