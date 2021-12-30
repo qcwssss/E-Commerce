3239427f66b5cb04f6855e7e688989220afb946d
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-29 19:11:06 -0800

    complete order route

--- ecommerce-api/routes/order.js
+++ ecommerce-api/routes/order.js
@@ -66,5 +66,32 @@ router.get('/', verifyTokenAndAdmin, async (req, res) => {
 });
 
 // GET MONTHLY INCOME
+router.get('/income', verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previosuMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+    try {
+        const income = await Order.aggregate([
+            { $match: { createdAt: { $gte: previosuMonth } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                    sales: "$amount",
+                },
+            },
+            {
+                $group: {
+                    _id: '$month',
+                    total: { $sum: '$sales'},
+                },
+            },
+        ]);
+        res.status(200).json(income);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
