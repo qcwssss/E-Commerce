14e6084a8f8d73dbb5878f5f918e26915ef6fa44
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-29 21:05:22 -0800

    add stripe api

--- ecommerce-api/routes/order.js
+++ ecommerce-api/routes/order.js
@@ -41,7 +41,7 @@ router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
         await Order.findByIdAndDelete(req.params.id);
         res.status(200).json('Order has been deleted...');
     } catch (err) {
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
 });
 
