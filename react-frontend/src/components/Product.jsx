942888b0acc05ccfb6cd148437a6937d5061fb57
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-06 09:46:01 -0800

    add hovering effect

--- react-frontend/src/components/Product.jsx
+++ react-frontend/src/components/Product.jsx
@@ -3,18 +3,38 @@ import {
   SearchOutlined,
   ShoppingCartOutlined,
 } from "@material-ui/icons";
-import React from "react";
 import styled from "styled-components";
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(255, 255, 255, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
 
 const Container = styled.div`
   flex: 1;
   margin: 5px;
+  min-width: 280px;
   height: 350px;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
@@ -30,21 +50,6 @@ const Image = styled.img`
   z-index: 2;
 `;
 
-const Info = styled.div`
-  opacity: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: all 0.5s ease;
-  cursor: pointer;
-`;
 const Icon = styled.div`
   width: 40px;
   height: 40px;
@@ -55,6 +60,10 @@ const Icon = styled.div`
   justify-content: center;
   margin: 10px;
   transition: all 0.5s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
 `;
 
 const Product = ({ item }) => {
