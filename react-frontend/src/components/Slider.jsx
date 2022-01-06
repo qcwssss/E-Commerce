040d5aeaf2e1779d929c1258d9d10dc4e5249b17
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-05 16:03:53 -0800

    add image container

--- react-frontend/src/components/Slider.jsx
+++ react-frontend/src/components/Slider.jsx
@@ -5,7 +5,6 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
 `;
 
@@ -27,12 +26,46 @@ const Arrow = styled.div`
   opacity: 0.5;
 `;
 
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
 const Slider = () => {
   return (
     <Container>
       <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://picsum.photos/200/300" />
+          </ImgContainer>
+          <InfoContainer></InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
