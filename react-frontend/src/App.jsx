4cf1b36d38a1a344df04ff08870142b16deddfca
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-07 11:46:01 -0800

    set app to home page

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
+// import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
+// import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
+// import Register from "./pages/Register";
 
 const App = () => {
-  return <Cart />;
+  return <Home />;
 };
 
 export default App;
