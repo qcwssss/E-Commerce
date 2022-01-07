f931441676640f89969500cdb7c9bc1eff73c0e8
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-06 20:18:22 -0800

    adding cart page

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,11 +1,12 @@
 // import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 // import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Register />;
+  return <Cart />;
 };
 
 export default App;
