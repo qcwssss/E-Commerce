a9048e7a647e986979282bdeee809005c1101480
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-06 20:09:47 -0800

    complete register page

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,10 +1,11 @@
 // import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
 // import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Login />;
+  return <Register />;
 };
 
 export default App;
