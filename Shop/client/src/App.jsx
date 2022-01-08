792b5366d8cea755d258a16c8243b52c2da166b9
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-07 21:25:31 -0800

    start product

--- Shop/client/src/App.jsx
+++ Shop/client/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/products/:category" element={<ProductList />} />
-        <Route path="/products/:id" element={<Product />} />
+        <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
