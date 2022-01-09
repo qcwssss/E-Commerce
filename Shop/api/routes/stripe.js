61e767b04524745fd5951cce90e38d0d97971988
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-08 21:43:25 -0800

    fixed strip key

--- Shop/api/routes/stripe.js
+++ Shop/api/routes/stripe.js
@@ -1,21 +1,28 @@
-const router = require('express').Router();
-const stripe = require('stripe')(process.env.STRIPE_KEY);
+const router = require("express").Router();
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const dotenv = require("dotenv");
+dotenv.config();
+const KEY = process.env.STRIPE_KEY;
+const stripe = require("stripe")(KEY);
 
-router.post('/payment', (req, res) => {
-    stripe.charges.create(
-        {
-            source: req.body.tokenId,
-            amount: req.body.amount,
-            currency: 'usd',
-        },
-        (stripeErr, stripeRes) => {
-            if (stripeErr) {
-                res.status(500).json(stripeErr);
-            } else {
-                res.status(200).json(stripeRes);
-            }
-        }
-    );
+router.post("/payment", (req, res) => {
+  //   console.log(KEY);
+  //   console.log(process.env.node_env);
+
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
 });
 
-module.exports = router
+module.exports = router;
